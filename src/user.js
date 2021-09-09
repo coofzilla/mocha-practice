c86f9faadf0f082fa9084bae126c97ecf846030d
Jay Hernandez <jeric614@yahoo.com>
2021-09-09 18:09:01 +0900

    test passed, test user added to atlas

--- src/user.js
+++ src/user.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   name: String,
 });
 
+//name of collection is string auto appends an s at end
 const User = mongoose.model("user", UserSchema);
 
 module.exports = User;
--- test/test_helper.js
+++ test/test_helper.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const keys = require("../config/dev");
 
 mongoose.connect(keys.mongoURI);
+
 mongoose.connection
   .once("open", () => console.log("Good to go!"))
   .on("error", (error) => {
