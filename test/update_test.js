b879df075b629f0fa7d11cfb166bd895a4d189dd
Jay Hernandez <jeric614@yahoo.com>
2021-09-10 11:31:14 +0900

    added update testing w/helper function

--- test/update_test.js
+++ test/update_test.js
@@ -9,13 +9,21 @@ describe("Updating records", async () => {
     await joe.save();
   });
 
+  const assertName = async (operation) => {
+    const users = await User.find();
+    assert(users.length === 1);
+    assert(users[0].name === "Alex");
+  };
+
   it("instance type using set and save", async () => {
     //set only persists the change in memory not to the db
     await joe.set("name", "Alex");
     //need to use save to db
-    await joe.save();
-    const users = await User.find();
-    assert(users.length === 1);
-    assert(users[0].name === "Alex");
+    assertName(await joe.save());
+  });
+
+  it("A model instance can update", async () => {
+    await joe.updateOne({ name: "Alex" });
+    assertName(await joe.save());
   });
 });
