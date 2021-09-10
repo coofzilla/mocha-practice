9d83c1a736878fcb93b00030da51074ffe70aba8
Jay Hernandez <jeric614@yahoo.com>
2021-09-10 10:35:12 +0900

    added delete testing

---
+++ test/delete_test.js
@@ -0,0 +1,37 @@
+const assert = require("assert");
+const { beforeEach } = require("mocha");
+const User = require("../src/user");
+
+describe("Deleting a user", async () => {
+  let joe;
+
+  beforeEach(async () => {
+    joe = await new User({ name: "Joe" });
+    await joe.save();
+  });
+
+  it("modal instance remove", async () => {
+    await joe.remove();
+    const user = await User.findOne({ name: "Joe" });
+    assert(user === null);
+  });
+
+  it("class method remove", async () => {
+    //delete a bunch of records w/criteria
+    await User.deleteMany({ name: "Joe" });
+    const user = await User.findOne({ name: "Joe" });
+    assert(user === null);
+  });
+
+  it("class method findAndRemove", async () => {
+    await User.findOneAndRemove({ name: "Joe" });
+    const user = await User.findOne({ name: "Joe" });
+    assert(user === null);
+  });
+
+  it("class method findByIdAndRemove", async () => {
+    await User.findByIdAndRemove(joe._id);
+    const user = await User.findOne({ name: "Joe" });
+    assert(user === null);
+  });
+});
--- test/reading_test.js
+++ test/reading_test.js
@@ -3,14 +3,23 @@ const User = require("../src/user");
 
 describe("Reading users out of the database", () => {
   let joe;
+
   beforeEach(async () => {
     joe = new User({ name: "Joe" });
-
     await joe.save();
   });
 
   it("finds all users with a name of joe", async () => {
-    const response = await User.find({ name: "Joe" });
-    console.log(response);
+    const users = await User.find({ name: "Joe" });
+    assert(users[0]._id.toString() === joe._id.toString());
+    //these are not actually same same; object holding string
+    // console.log(users[0]._id);
+    // console.log(joe._id);
+  });
+
+  it("finds a user with a particular id", async () => {
+    //mongoose expects object for criteria of search
+    const user = await User.findOne({ _id: joe._id });
+    assert(user.name === "Joe");
   });
 });
