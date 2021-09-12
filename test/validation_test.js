ebe8ca7b542db89a0cbc33dc7b91e3b5892c1251
Jay Hernandez <jeric614@yahoo.com>
2021-09-12 21:00:31 +0900

    finished validation with try/catch

--- test/validation_test.js
+++ test/validation_test.js
@@ -8,4 +8,22 @@ describe("Validating records", async () => {
     const { message } = validationResult.errors.name;
     assert(message === "Name is required");
   });
+  //validateSync is from mongoose
+  it("requires a user name longer than 2 characters", () => {
+    const user = new User({ name: "Al" });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === "Name must be longer than 2 characters");
+  });
+
+  it("disallows invalid records from being saved", async () => {
+    try {
+      const user = new User({ name: "Al" });
+      await user.save();
+    } catch (validationResult) {
+      const { message } = validationResult.errors.name;
+      assert(message === "Name must be longer than 2 characters");
+    }
+  });
 });
